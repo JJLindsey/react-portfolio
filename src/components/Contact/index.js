0fb64ad36acd94bb4507d0d1ada69f768d1da300
JJLindsey <jlindsey010@gmail.com>
2021-08-26 16:33:40 -0700

    add components and react bootstrap

---
+++ src/components/Contact/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact(props) {
+    return (
+        <div>
+            This is the Contact page
+            
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/components/Nav/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div>
+            This is the Navbar
+        </div>
+    )
+}
+
+export default Nav
