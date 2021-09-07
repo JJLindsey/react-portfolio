95925a58e354fda452758757b9c88de9850e2c2b
JJLindsey <jlindsey010@gmail.com>
2021-09-07 08:49:22 -0700

    resume component & CSS

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 function Home() {
     return (
-        <div class="main">
-            <div class="bg"></div>
-                <div class="text">
-                jennifer lindsey
-                <h1>web developer</h1>
+        <div class='home'>
+            <div class="main">
+                <div class="bg"></div>
+                    <div class="text">
+                    jennifer lindsey
+                    <h1>web developer</h1>
+                </div>
             </div>
         </div>
     )
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,41 +4,43 @@ import { VscMilestone } from 'react-icons/vsc'
 
 const Resume = () => {
     return (
-        <container class='container-fluid' class='resume'>
-            <div id='resume'>
-                <h1> <BiCode/> Resume <BiCodeAlt/></h1>
-                <h3> Jennifer Lindsey </h3>
+        <div class='container-fluid' id='resume'>
+                <h2> <BiCode/> Resume <BiCodeAlt/></h2>
+                <div class='container' id='resume'>
+                    <h3> Jennifer Lindsey </h3>
                     <section id='resume-info'>
-                    <p>Front End Web Developer with a background in Art and a dedication to life-long learning. Effective at combining creativity and problem solving to develop user-friendly applications. Experience in collaborating to create and deliver materials that are accessible to all audiences.
-                    </p>
+                        <p>Front End Web Developer with a background in Art and a dedication to life-long learning. Effective at combining creativity and problem solving to develop user-friendly applications. Experience in collaborating to create and deliver materials that are accessible to all audiences.
+                        </p>
                     </section>
-                <h4><VscMilestone/> Technical Skills</h4>
-                <h4>Front End</h4>
-                    <ul>
-                        <li>Javascript</li>
-                        <li>HTML5</li>
-                        <li>CSS</li>
-                        <li>Bootstrap</li>
-                        <li>React</li>
-                    </ul>
-                <h4>Back End</h4>
-                    <ul>
-                        <li>NodeJS</li>
-                        <li>Express</li>
-                        <li>MongoDB</li>
-                        <li>Mongoos</li>
-                        <li>GraphQL</li>
-                    </ul>
-                <h4>Education</h4>
-                <section id='resume-ed'>
-                Vanderbilt University Bootcamp Certificate
-                <p>A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3, JavaScript, jQuery, Bootstrap, Firebase, Node Js, MySQL, MongoDB, Express, Handelbars.js, GraphQL & ReactJS.
-                </p>
-                <p>MA Art History - Courtauld Institute of Art, London, England</p>
-                <p>BA Art History - University of Texas, Arlington</p>
-                </section>
-            </div>
-        </container>
+                    <h4><VscMilestone/> Technical Skills</h4>
+                    <section id='resume-info'>
+                        <h4>Front End</h4>
+                        <ul>
+                            <li>Javascript</li>
+                            <li>HTML5</li>
+                            <li>CSS</li>
+                            <li>Bootstrap</li>
+                            <li>React</li>
+                        </ul>
+                        <h4>Back End</h4>
+                        <ul>
+                            <li>NodeJS</li>
+                            <li>Express</li>
+                            <li>MongoDB</li>
+                            <li>Mongoos</li>
+                            <li>GraphQL</li>
+                        </ul>
+                    </section>
+                    <h4>Education</h4>
+                    <section id='resume-info'>
+                        <p>Vanderbilt University Bootcamp Certificate</p>
+                        <p>A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3, JavaScript, jQuery, Bootstrap, Firebase, Node Js, MySQL, MongoDB, Express, Handelbars.js, GraphQL & ReactJS.
+                        </p>
+                        <p>MA Art History - Courtauld Institute of Art, London, England</p>
+                        <p>BA Art History - University of Texas, Arlington</p>
+                    </section>
+                </div>
+        </div>
     )
 }
 
